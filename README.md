8ff390c300be6e9e39398761ca6f5e13154cff8d
Bernard Ograh <bernard.ograh@amalitechtraining.org>
2024-09-16 13:06:59 +0000

    Remove previous project

--- README.md
+++
@@ -1,6 +0,0 @@
-Gmail --> springauth67@gmail.com : magicauth456
-API Credentials --> 
-  {
-		"username": "springauth67",
-		"password": "password"
-   }
--- src/main/java/com/example/Demo/repository/TokenRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.Demo.repository;
-
-import com.example.Demo.entity.Token;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TokenRepository extends JpaRepository<Token, String> {
-}
